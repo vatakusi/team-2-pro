e2502316d9e67ae821bc8631ab5a5983ba4a5102
vatakusi <newsskyx@gmail.com>
2022-01-11 09:54:32 +0000

    Deploying to gh-pages from @ vatakusi/team-2-pro@f8240f51e115df1b0b4f93ad3916667d9d6b1f60 🚀

--- src.5deae462.js
+++ src.5deae462.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-2-pro/src.2b090e36.js.map
+//# sourceMappingURL=/team-2-pro/src.5deae462.js.map
