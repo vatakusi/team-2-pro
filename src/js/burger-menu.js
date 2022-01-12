e4b179324c009e6578be7fd1bc4458f9cf642e72
Ihor <ihorfront88@gmail.com>
2022-01-12 22:12:19 +0200

    fixed bug with burger menu and added js code for burger links

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,6 +1,7 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
+    const mobileLink = document.querySelector("[data-menu-link]");
 
     menuBtnRef.addEventListener("click", () => {
         const expanded =
@@ -11,5 +12,11 @@
 
         document.body.classList.toggle("modal-open");
         mobileMenuRef.classList.toggle("active");
+    });
+
+    mobileLink.addEventListener("click", () => {
+        mobileMenuRef.classList.remove("active");
+        document.body.classList.remove("modal-open");
+        menuBtnRef.classList.remove("active");
     });
 })();
