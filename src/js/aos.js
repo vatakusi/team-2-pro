f5a341eeda188032fbbe8399bb81682bd305eab7
Alexandr Morozov // chillipepper <newsskyx@gmail.com>
2022-01-12 23:38:22 +0200

    top up

--- src/js/aos.js
+++ src/js/aos.js
@@ -1,4 +1,4 @@
-AOS.init({
+/* AOS.init({
    // Global settings:
    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
@@ -18,4 +18,4 @@ AOS.init({
    once: false, // whether animation should happen only once - while scrolling down
    mirror: false, // whether elements should animate out while scrolling past them
    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-});
+}); */
