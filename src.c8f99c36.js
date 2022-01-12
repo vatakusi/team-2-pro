4a3831ce731046c1da3a34d47072660925e5ee2a
vatakusi <newsskyx@gmail.com>
2022-01-12 08:17:47 +0000

    Deploying to gh-pages from @ vatakusi/team-2-pro@a7ec316abf0c040b0f09600567ba79ac4f281411 🚀

--- src.c8f99c36.js
+++ src.c8f99c36.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/about-images/mobile-about-bg.png":[["mobile-about-bg.f8efff91.png","cQaO"],"cQaO"],"./../images/about-images/mobile-about-bg2x.png":[["mobile-about-bg2x.9d3f9eb0.png","t1Oi"],"t1Oi"],"./../images/about-images/desk-about-bg.png":[["desk-about-bg.3a6ccbf8.png","Gnau"],"Gnau"],"./../images/about-images/desk-about-bg2x.png":[["desk-about-bg2x.70fb8bf7.png","uFER"],"uFER"],"./../images/advantages-images/advantages-icon1.png":[["advantages-icon1.28b56f7e.png","adVF"],"adVF"],"./../images/advantages-images/advantages-icon2.png":[["advantages-icon2.7b20a05f.png","pMWd"],"pMWd"],"./../images/advantages-images/advantages-icon3.png":[["advantages-icon3.c76fbf7b.png","wWxb"],"wWxb"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-2-pro/src.1c81ae1b.js.map
+//# sourceMappingURL=/team-2-pro/src.c8f99c36.js.map
