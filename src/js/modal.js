fa44455775f914892066e50cc093cf1f54028b36
PY2512 <58562352+PY2512@users.noreply.github.com>
2022-01-13 02:22:56 +0200

    complated
    Доделал модалки, пофиксил мелочи, html на валидаторе валидится без ошибок

--- src/js/modal.js
+++ src/js/modal.js
@@ -12,3 +12,18 @@
       refs.modal.classList.toggle('is-hidden');
     }
   })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-map-open]'),
+      closeModalBtn: document.querySelector('[data-map-close]'),
+      modal: document.querySelector('[data-map]'),
+      };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
+  })();
