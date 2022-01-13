b95e3c1ac4aa5c10c34037c5a1aead3a1c5cba88
Ihor <ihorfront88@gmail.com>
2022-01-13 18:00:41 +0200

    added modal to header

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,43 +1,49 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
-    const mobileLink0 = document.querySelector("[data-menu-link0]");
-    const mobileLink1 = document.querySelector("[data-menu-link1]");
-    const mobileLink2 = document.querySelector("[data-menu-link2]");
-    const mobileLink3 = document.querySelector("[data-menu-link3]");
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+  const mobileLink0 = document.querySelector('[data-menu-link0]');
+  const mobileLink1 = document.querySelector('[data-menu-link1]');
+  const mobileLink2 = document.querySelector('[data-menu-link2]');
+  const mobileLink3 = document.querySelector('[data-menu-link3]');
 
-    menuBtnRef.addEventListener("click", () => {
-        const expanded =
-            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+  menuBtnRef.addEventListener('click', () => {
+    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
-        menuBtnRef.classList.toggle("active");
-        menuBtnRef.setAttribute("aria-expanded", !expanded);
+    menuBtnRef.classList.toggle('active');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
 
-        document.body.classList.toggle("modal-open");
-        mobileMenuRef.classList.toggle("active");
-    });
+    document.body.classList.toggle('modal-open');
+    mobileMenuRef.classList.toggle('active');
+  });
 
-    mobileLink0.addEventListener("click", () => {
-        mobileMenuRef.classList.remove("active");
-        document.body.classList.remove("modal-open");
-        menuBtnRef.classList.remove("active");
-    });
-    mobileLink1.addEventListener("click", () => {
-        mobileMenuRef.classList.remove("active");
-        document.body.classList.remove("modal-open");
-        menuBtnRef.classList.remove("active");
-    });
-    mobileLink2.addEventListener("click", () => {
-        mobileMenuRef.classList.remove("active");
-        document.body.classList.remove("modal-open");
-        menuBtnRef.classList.remove("active");
-    });
-    mobileLink3.addEventListener("click", () => {
-        mobileMenuRef.classList.remove("active");
-        document.body.classList.remove("modal-open");
-        menuBtnRef.classList.remove("active");
-    });
+  mobileLink0.addEventListener('click', removeMenu);
+  mobileLink1.addEventListener('click', removeMenu);
+  mobileLink2.addEventListener('click', removeMenu);
+  mobileLink3.addEventListener('click', removeMenu);
+
+  const refs = {
+    openBuyBtn: document.querySelector('[data-buy-btn="btn"]'),
+    openBuyBtnMenu: document.querySelector('[data-buy-btn="btn-menu"]'),
+    closeBuyBtn: document.querySelector('[data-buy-close]'),
+    modalBuy: document.querySelector('[data-modal-buy]'),
+  };
+
+  refs.openBuyBtn.addEventListener('click', toggleModal);
+  refs.openBuyBtnMenu.addEventListener('click', () => {
+    refs.modalBuy.classList.toggle('is-hidden');
+    mobileMenuRef.classList.remove('active');
+    menuBtnRef.classList.remove('active');
+  });
+  refs.closeBuyBtn.addEventListener('click', toggleModal);
+
+  function removeMenu() {
+    mobileMenuRef.classList.remove('active');
+    document.body.classList.remove('modal-open');
+    menuBtnRef.classList.remove('active');
+  }
+
+  function toggleModal() {
+    refs.modalBuy.classList.toggle('is-hidden');
+    document.body.classList.toggle('modal-open');
+  }
 })();
-
-
-
